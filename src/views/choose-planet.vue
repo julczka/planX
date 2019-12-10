a3799de1db4055da17f612ef07ba2b5ece1d5572
julczka <56249914+julczka@users.noreply.github.com>
2019-12-10 17:12:25 +0100

    styled selector

--- src/views/choose-planet.vue
+++ src/views/choose-planet.vue
@@ -1,115 +1,93 @@
 <template>
   <div class="about">
+    <div class="choice-row">
+      <div class="choice-container">
+        <div class="choice-container-color">
+          <div class="choice-container-text">
+            <h1>Choose your destination</h1>
+          </div>
 
-<div class="choice-row">
-  <div class="choice-container">
-
-    <div class="choice-container-text">
-      <h1>Choose your destination</h1>
-    </div>
-
-    <div class="selector-container">
-       <multiselect
-      v-model="selectedPlanet"
-      :options="planets"
-      placeholder="Choose your destination"
-      :allow-empty="false"
-      :showLabels="false"
-      :preselectFirst="true"
-      class="selector"
-      
-    >
-    </multiselect>
-
-      <router-link :to="selectedPlanet"
->
-     <b-button
-       class="purple_hover"
-       @click.native="persist"
-       >Go to planet</b-button></router-link>
-
-
-    </div>
-
-
-  </div>
-
-    <div class="img-container">
-
-
-                  <parallax-container class="container">
-            <parallax-element
-           
-              :parallaxStrength="-5"
-              type="rotation"
-              tag="img"
-              class="absolute"
-              :src="img1"
+          <div class="selector-container">
+            <multiselect
+              v-model="selectedPlanet"
+              :options="planets"
+              placeholder="Choose your destination"
+              :allow-empty="false"
+              :showLabels="false"
+              :preselectFirst="true"
+              class="selector"
             >
-            </parallax-element>
+            </multiselect>
 
-            <parallax-element
-              :parallaxStrength="5"
-              type="rotation"
-              tag="img"
-              class="absolute"
-              :src="img2"
+            <router-link :to="selectedPlanet">
+              <b-button class="purple_hover" @click.native="persist"
+                >Go to planet</b-button
+              ></router-link
             >
-            </parallax-element>
+          </div>
+        </div>
+      </div>
 
+      <div class="img-container">
+        <parallax-container class="container">
+          <parallax-element
+            :parallaxStrength="-5"
+            type="rotation"
+            tag="img"
+            class="absolute"
+            :src="img1"
+          >
+          </parallax-element>
+
+          <parallax-element
+            :parallaxStrength="5"
+            type="rotation"
+            tag="img"
+            class="absolute"
+            :src="img2"
+          >
+          </parallax-element>
+
+          <parallax-element
+            :parallaxStrength="-10"
+            type="depth"
+            tag="img"
+            class="absolute"
+            :src="img3"
+            id="kurwa"
+          >
+          </parallax-element>
+
+          <transition name="puff">
             <parallax-element
-              :parallaxStrength="-10"
+              v-if="show"
+              :parallaxStrength="10"
               type="depth"
               tag="img"
               class="absolute"
-              :src="img3"
+              :src="planetImage"
+              :key="planetImage"
               id="kurwa"
             >
             </parallax-element>
+          </transition>
 
-                  <transition name="puff" >
-
-                        <parallax-element
-                         v-if="show"
-                          :parallaxStrength="10"
-                          type="depth"
-                          tag="img"
-                          class="absolute"
-                          :src="planetImage"
-                          :key="planetImage"
-                          id="kurwa"
-                        >
-                        </parallax-element>
-                  </transition>
-            
-
-            
-
-            <parallax-element
-              :parallaxStrength="20"
-              type="rotation"
-              tag="img"
-              class="absolute"
-              :src="img5"
-              id="kurwa"
-            >
-            </parallax-element>
-          </parallax-container>
-
-
-
-
-
+          <parallax-element
+            :parallaxStrength="20"
+            type="rotation"
+            tag="img"
+            class="absolute"
+            :src="img5"
+            id="kurwa"
+          >
+          </parallax-element>
+        </parallax-container>
+      </div>
+    </div>
+    <span> Selected {{ selectedPlanet }} </span>
   </div>
-  
 
-
-  </div>
-<span> Selected {{ selectedPlanet }} </span>
-</div>
-
-
-    <!-- <h1>I'm going to:</h1>
+  <!-- <h1>I'm going to:</h1>
     <select v-model="selectedPlanet">
     <option disabled value="">Please select one</option>
     <option>Proxima Centauri B</option>
@@ -125,46 +103,34 @@
 
 
    -->
-
-
- 
-
-
-
-
 </template>
 
 <script>
 import Multiselect from "vue-multiselect";
-import blinkingButton from '@/components/blinkingButton.vue'
+import blinkingButton from "@/components/blinkingButton.vue";
 import { ParallaxContainer, ParallaxElement } from "vue-mouse-parallax";
 
-
 export default {
-
-components: {
+  components: {
     Multiselect,
-    'b-button':blinkingButton,
-     ParallaxContainer,
-    ParallaxElement,
+    "b-button": blinkingButton,
+    ParallaxContainer,
+    ParallaxElement
   },
 
-  
+  data() {
+    return {
+      show: true,
+      selectedPlanet: "",
+      planets: ["Gliese 667Cc", "Kepler-22b", "Proxima Centauri B"],
+      img: require("@/assets/planets/Earth.jpg"),
 
-   data () { 
-     return {
-    show: true,
-    selectedPlanet: '',
-    planets: ["Gliese 667Cc", "Kepler-22b", "Proxima Centauri B"],
-    img: require('@/assets/planets/Earth.jpg'),
-
-    img1: require("@/assets/planet_choice/1.png"),
+      img1: require("@/assets/planet_choice/1.png"),
       img2: require("@/assets/planet_choice/2.png"),
       img3: require("@/assets/planet_choice/3.png"),
       //img4: require("@/assets/planet_choice/4.png"),
       img5: require("@/assets/planet_choice/5.png")
-
-    }
+    };
   },
 
   computed: {
@@ -181,13 +147,10 @@ components: {
     }
   },
 
-
   mounted() {
     if (localStorage.selectedPlanet) {
       this.selectedPlanet = localStorage.selectedPlanet;
     }
-
-    
   },
   watch: {
     selectedPlanet(newselectedPlanet) {
@@ -198,18 +161,15 @@ components: {
   methods: {
     persist() {
       localStorage.selectedPlanet = this.selectedPlanet;
-      console.log(localStorage.selectedPlanet + ' localstorage Planet');
-      console.log(this.selectedPlanet + ' this planet');
+      console.log(localStorage.selectedPlanet + " localstorage Planet");
+      console.log(this.selectedPlanet + " this planet");
       console.log(name);
-    },
-
-    
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
-
 @import "@/styles/_multiselect.scss";
 
 .choice-row {
@@ -217,10 +177,10 @@ components: {
   align-items: center;
   justify-content: space-around;
   height: 90vh;
+  
 }
 
 .choice-container {
-  
   height: 100%;
   width: 40%;
   display: flex;
@@ -228,28 +188,38 @@ components: {
   justify-content: space-around;
   flex-direction: column;
 
-  .selector-container {
-    width: 50%;
+  &-color {
     display: flex;
   align-items: center;
   justify-content: space-around;
   flex-direction: column;
+  height: 70%;
+  width: 80%;
+  
+  }
+
+  .selector-container {
+    width: 80%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: column;
 
     .selector {
-      margin: 1em;
+      margin: 2em;
     }
-
   }
 
   &-text {
-
     width: 90%;
 
+    h1 {
+      font-size: 3rem;
+    }
   }
 }
 
 .img-container {
-  
   height: 100%;
   width: 60%;
   display: flex;
@@ -274,14 +244,12 @@ components: {
   justify-content: center;
 }
 
-
-
 .puff-enter-active {
-	animation: puff-in-center 0.6s cubic-bezier(0.470, 0.000, 0.745, 0.715) both;
+  animation: puff-in-center 0.6s cubic-bezier(0.47, 0, 0.745, 0.715) both;
 }
 
 .puff-leave-active {
-	animation: puff-out-center 0.6s cubic-bezier(0.165, 0.840, 0.440, 1.000) both;
+  animation: puff-out-center 0.6s cubic-bezier(0.165, 0.84, 0.44, 1) both;
 }
 
 @keyframes puff-in-center {
@@ -309,7 +277,4 @@ components: {
     opacity: 0;
   }
 }
-
-
-
 </style>
