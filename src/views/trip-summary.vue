5bfe55a2e0b6b24e582d30b6f5e14a3928753248
julczka <56249914+julczka@users.noreply.github.com>
2019-12-10 10:03:06 +0100

    script return

--- src/views/trip-summary.vue
+++ src/views/trip-summary.vue
@@ -67,6 +67,33 @@
 </template>
 
 
+<script>
+export default {
+  data () { 
+     return {
+    selectedShip: '',
+    planetName: '',
+    }
+  },
+  mounted() {
+    if (localStorage.selectedShip) {
+      this.selectedShip = localStorage.selectedShip;
+    }
+     if (localStorage.planetName) {
+      this.planetName = localStorage.planetName;
+    }
+  },
+  watch: {
+    selectedShip(newselectedShip) {
+      localStorage.selectedShip = newselectedShip;
+    },
+    planetName(newPlanetName) {
+      localStorage.planetName = newPlanetName;
+    }
+  },
+}
+</script>
+
 
 <style lang="scss">
 
